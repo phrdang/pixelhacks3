d3cb231fa67dd09517d064614cf0fadb6c39e231
Sonia <sonia.sharapova@yahoo.com>
2019-01-26 12:58:21 -0800

    Added function to check password

---
+++ index.js
@@ -0,0 +1,64 @@
+var  capLetter = 0;
+var lowLetter = 0;
+var numInPassword = 0;
+var specialChar = 0;
+var consecutive = 0;
+
+function validPassword()
+{
+var password = prompt("Please enter your password");
+var len = password.length;
+
+var bigLetters = /^[A-Z]*$/;
+var smallLetters = /^[a-z]*$/;
+var numbers = /^[0-9]*$/; 
+var specialChar = ;
+
+if (len < 8)
+{
+    System.out.println("Password is too short.");
+}
+
+var bigLetters = /^[A-Z]*$/;
+var smallLetters = /^[a-z]*$/;
+var numbers = /^[0-9]*$/; 
+
+var i;
+for (i = 0; i < len; i++) 
+{ 
+  if(password.substring(i, i + 1) == bigLetters)
+  {
+    capLetter++;
+  }
+  else if (password.substring(i, i + 1) == smallLetters)
+  {
+    lowLetter++;
+  }
+  else if (password.substring(i, i + 1) == numbers)
+  {
+    numInPassword++;
+  }
+  else if (password.substring(i, i+1) == "!"||password.substring(i, i+1) == "@"||password.substring(i, i+1) == "#"|| password.substring(i, i+1) =="$"
+  || password.substring(i, i+1) == "%"||password.substring(i, i+1) == "^"||password.substring(i, i+1) == "&"|| password.substring(i, i+1) =="*"||
+  password.substring(i, i+1) == "("||password.substring(i, i+1) == ")"||password.substring(i, i+1) == "-" || password.substring(i, i+1) == "+"||
+  password.substring(i, i+1) == "_"|| password.substring(i, i+1) == "="||password.substring(i, i+1) == "{"||password.substring(i, i+1) == "}"||
+  password.substring(i, i+1) == "["||password.substring(i, i+1) == "]"||password.substring(i, i+1) == "|"||password.substring(i, i+1) == ":"||
+  password.substring(i, i+1) == ";" || password.substring(i, i+1) == "'"||password.substring(i, i+1) == "<"||password.substring(i, i+1) == ">"||
+  password.substring(i, i+1) == "?"||password.substring(i, i+1) == "`"||password.substring(i, i+1) == "~")
+  {
+    specialChar++;
+  }
+
+}
+
+if(capLetter == 0 || lowLetter == 0 || numInPassword == 0 || specialChar == 0 )
+{
+    System.out.println("Invalid Password");
+}
+else if (capLetter !== 0 || lowLetter !== 0 || numInPassword !== 0 || specialChar !== 0 )
+{
+    System.out.println("Valid Password");
+  
+  }
+        
+}
