fdca3070c7fc8648d7deb3516bf221ac5792a1cb
Sonia <sonia.sharapova@yahoo.com>
2019-01-26 14:05:17 -0800

    new change

--- index.js
+++ index.js
@@ -41,7 +41,13 @@ for (i = 0; i < len; i++)
 
 }
 
-if(capLetter !== 0 && lowLetter !== 0 && numInPassword !== 0 && specialChar !== 0 && len == 8)
+//if(capLetter !== 0 && lowLetter !== 0 && numInPassword !== 0 && specialChar !== 0 && len == 8)
+//{
+ //   document.getElementById('passwordOutput').innerHTML = "Valid Password";
+    
+//}
+
+if(capLetter !== 0 && lowLetter !== 0)
 {
     document.getElementById('passwordOutput').innerHTML = "Valid Password";
     
